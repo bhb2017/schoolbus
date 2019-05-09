a91c24494120b12c139bab808d9d694a7d8a4b0d
cxq <1986541495@qq.com>
2019-05-09 18:41:27 +0800

    初始化项目

---
+++ ShoolBus/settings/develop.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shoolbus',
+        'USER': 'root',
+        'PASSWORD': 'qq123321',
+        'HOST': 'localhost',
+        'PORT': '3307',
+    }
+}
